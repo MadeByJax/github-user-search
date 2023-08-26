6e071e99526e9d073b0a39d8405b4a6c91bf7e2e
MadeByJax <jaxzynfollett@outlook.com>
2023-08-26 17:00:49 +1000

    Modified website responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Card from "./components/Card";
+import { Octokit } from "octokit";
 
 function App() {
+  const octokit = new Octokit({
+    auth: "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",
+  });
+
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const fetchGithubUser = async (searchedUser) => {
+    try {
+      setIsLoading(true);
+      const response = await octokit.request("GET /users/{username}", {
+        username: searchedUser,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      });
+      if (response.status === 200) {
+        console.log(response);
+        setUser(response.data);
+        setIsLoading(false);
+        setError();
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      console.log(user);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGithubUser("Madebyjax");
+  }, []);
+
   return (
     <div className="flex items-center justify-center md:h-screen px-6 py-8">
-      <div className="flex flex-col max-w-3xl">
+      <div className="w-[90%]  md:w-[730px]">
         <Header />
-        <Search />
-        <Card />
+        <Search error={error} fetchGithubUser={fetchGithubUser} />
+        <Card isLoading={isLoading} user={user} />
       </div>
     </div>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
-const Search = () => {
+const Search = ({ fetchGithubUser, error }) => {
+  const [searchedUser, setSearchedUser] = useState();
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    fetchGithubUser(searchedUser);
+  };
+
+  console.log(error);
+
   return (
-    <form className="gap-2 bg-card-blue flex p-4  items-center mt-10 rounded-lg">
+    <form
+      onSubmit={onSubmitHandler}
+      className="gap-2 bg-card-blue flex p-4  items-center mt-10 rounded-lg"
+    >
       <svg height="24" width="24" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M10.609 0c5.85 0 10.608 4.746 10.608 10.58 0 2.609-.952 5-2.527 6.847l5.112 5.087a.87.87 0 01-1.227 1.233l-5.118-5.093a10.58 10.58 0 01-6.848 2.505C4.759 21.16 0 16.413 0 10.58 0 4.747 4.76 0 10.609 0zm0 1.74c-4.891 0-8.87 3.965-8.87 8.84 0 4.874 3.979 8.84 8.87 8.84a8.855 8.855 0 006.213-2.537l.04-.047a.881.881 0 01.058-.053 8.786 8.786 0 002.558-6.203c0-4.875-3.979-8.84-8.87-8.84z"
           fill="#0079ff"
         />
       </svg>
+
       <input
-        className="flex-1 bg-transparent text-xs w-auto"
-        placeholder="Search GitHub username..."
+        className="text-white flex-auto bg-transparent text-xs w-1"
         type="text"
+        value={searchedUser}
+        placeholder="Search Github username..."
+        onChange={(e) => {
+          setSearchedUser(e.target.value);
+          console.log(searchedUser);
+        }}
       />
+      {error && <p className="text-xs text-red-500 mr-4">No results</p>}
       <button className="text-s text-white bg-accent-blue px-4 py-2 rounded-lg">
         Search
       </button>
