89e1a3d344fd39ac3fdf01c0a65db797dbfb07c1
MadeByJax <jaxzynfollett@outlook.com>
2023-09-09 14:30:28 +1000

    adjusted css

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
   };
 
   return (
-    <div className="flex md:items-center justify-center bg-app-light dark:bg-app-dark h-full md:h-screen px-6 py-8">
+    <div className="flex md:items-center justify-center bg-app-light dark:bg-app-dark h-screen px-6 py-8">
       <div className="w-[90%]  md:w-[730px]">
         <Header handleThemeSwitch={handleThemeSwitch} theme={theme} />
         <Search
